eb17cbcecc6f0afbe6ca0931bbffb27a317419d9
jomapormentilla <jomapormentilla@gmail.com>
2020-10-28 14:27:48 -0400

    commit tree bddced77ec34e049772923f1827a0224633f2386

---
+++ lib/chatty_student.rb
@@ -0,0 +1,17 @@
+require_relative "./student.rb"
+
+class ChattyStudent < Student
+
+    def hello
+        super
+        puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+
+        def raise_hand
+            counter = 0
+            while counter < 10
+                super
+                counter += 1
+            end
+        end
+    end
+end
